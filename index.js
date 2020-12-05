a0902bd9af22a643646019c9838f664da61a1732
tdpaul315@gmail.com <tiaunaj.tsg@gmail.com>
2020-12-04 20:26:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution in this file!
+// Creates Object 
+
+const driver = {
+    name: 'Mary', 
+    address: '11 My Street'
+};
+
+// Non-Destructively Updates Object Using (Object.assign)
+
+function updateDriverWithKeyAndValue(obj, key, value ) {
+    return Object.assign({}, obj, {[key]: value}, {[key]: value});
+
+}
+
+const newDriver = updateDriverWithKeyAndValue(driver, 'name', 'Sam');
+
+// Destructively Updates Object
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+    obj[key] = value; 
+
+    return obj;
+}
+
+const updatedDriver = destructivelyUpdateDriverWithKeyAndValue(driver, 'address', '11 Broadway');
+
+// Deletes Object Non-Destructively 
+
+function deleteFromDriverByKey(obj, key) {
+    let newNewDriver = Object.assign({}, driver)
+    delete newNewDriver.name 
+
+    return newNewDriver
+}
+
+deleteFromDriverByKey(driver, 'name')
+
+
+// Deletes Object Destructively 
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+    delete obj.name;
+    return obj; 
+}
+
+destructivelyDeleteFromDriverByKey(driver, 'name')
